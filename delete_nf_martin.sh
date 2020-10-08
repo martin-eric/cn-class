e99128755720e83e33b92622774a761c6626f53e
ericsson <martin.chabot@ericsson.com>
2020-10-08 23:31:44 +0000

    modified delete_nf

--- delete_nf_martin.sh
+++ delete_nf_martin.sh
@@ -1,3 +1,5 @@
+echo "Deleting all HELM releases nameds martin"
+
 for i in `helm list | grep martin | awk '{print $1}'` ; do echo "Deleting HELM RELEASE $i" ; helm del --purge $i ; done
 while kubectl get pods --all-namespaces | grep martin | grep -i terminating 
 do
@@ -6,3 +8,5 @@ sleep 5
 done
 for j in `kubectl get ns | grep martin | awk '{print $1}'` ; do echo "Deleting NAMESPACE $j" ; kubectl delete ns $j ; done
 
+
+echo "FINISHED Deleting all HELM releases nameds martin"
