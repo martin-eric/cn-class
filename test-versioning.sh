1d90f5ac6925340e02678bd82aed15ffee9f935b
martin-eric <72566180+martin-eric@users.noreply.github.com>
2023-02-09 09:08:12 -0600

    Update test-versioning.sh

--- test-versioning.sh
+++ test-versioning.sh
@@ -1,4 +1,4 @@
-SCRIPTV="0.3"
+SCRIPTV="0.4"
 FILE=.swapoff
 FILE2=.binariesdone
 FILE3=.containerdworkarounddone
@@ -38,7 +38,7 @@ sudo rm -rf /etc/containerd/config.toml
 
 #Install containerd from docker registry
 
-VERSION=1.6.4-1
+VERSION=1.6.16-1
 sudo apt-get update 
 sudo apt-get install -y --allow-downgrades containerd.io=$VERSION 
 sudo apt-mark hold containerd.io
