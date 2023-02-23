95e20503666d5b048cc8f736c418ec1e51b1391a
martin-eric <72566180+martin-eric@users.noreply.github.com>
2023-02-23 13:51:53 -0600

    Update install_nocentino_jan2023.sh

--- install_nocentino_jan2023.sh
+++ install_nocentino_jan2023.sh
@@ -1,8 +1,12 @@
-SCRIPTV="0.92"
+SCRIPTV="0.93"
 FILE=.swapoff
 FILE2=.binariesdone
 
+echo "**********************************"
 echo "Starting script version $SCRIPTV"
+echo "**********************************"
+echo
+echo
 
 if [[ $EUID -ne 0 ]]; then
   echo "You must be a root user. Use SUDO " 2>&1
@@ -88,75 +92,75 @@ net.ipv4.ip_forward                 = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 EOF
 
-  #Apply sysctl params without reboot
-  sudo sysctl --system
+#Apply sysctl params without reboot
+sudo sysctl --system
 
-  ## How to fix kubernetes cluster start-up after containerd updated on ubuntu repository
-  ## version 1.59 does not play well with any kubernetes 1.2x version
-  ## need to remove and download latest containerd binary from a difference repository, I decided to use docker repo
-  ## To be done on both CP and worker nodes
+## How to fix kubernetes cluster start-up after containerd updated on ubuntu repository
+## version 1.59 does not play well with any kubernetes 1.2x version
+## need to remove and download latest containerd binary from a difference repository, I decided to use docker repo
+## To be done on both CP and worker nodes
 
-  #Add Docker’s official GPG key:
+#Add Docker’s official GPG key:
 
-  sudo mkdir -p /etc/apt/keyrings
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-  #Use the following command to set up the repository:
+#Use the following command to set up the repository:
 
-  echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   
-  #Remove containerd if already installed
+#Remove containerd if already installed
 
-  sudo apt-mark unhold containerd
-  sudo apt remove -y containerd
-  sudo rm -rf /etc/containerd/config.toml
+sudo apt-mark unhold containerd
+sudo apt remove -y containerd
+sudo rm -rf /etc/containerd/config.toml
 
-  #Install containerd from docker registery
+#Install containerd from docker registery
 
-  #Install containerd from docker registry
-  sudo apt-get update 
-  sudo apt-get install -y containerd.io
+#Install containerd from docker registry
+sudo apt-get update 
+sudo apt-get install -y containerd.io
 
-  sudo apt-mark hold containerd.io
+sudo apt-mark hold containerd.io
 
-  #Configure containerd
-  sudo mkdir -p /etc/containerd
-  sudo containerd config default | sudo tee /etc/containerd/config.toml
+#Configure containerd
+sudo mkdir -p /etc/containerd
+sudo containerd config default | sudo tee /etc/containerd/config.toml
 
-  #You can use sed to swap in true
-  sudo sed -i 's/            SystemdCgroup = false/            SystemdCgroup = true/' /etc/containerd/config.toml
+#You can use sed to swap in true
+sudo sed -i 's/            SystemdCgroup = false/            SystemdCgroup = true/' /etc/containerd/config.toml
 
-  #Restart containerd with the new configuration
-  sudo systemctl restart containerd
+#Restart containerd with the new configuration
+sudo systemctl restart containerd
 
-  #Install Kubernetes packages - kubeadm, kubelet and kubectl
-  #Add Google's apt repository gpg key
-  curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+#Install Kubernetes packages - kubeadm, kubelet and kubectl
+#Add Google's apt repository gpg key
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
-  #Add the Kubernetes apt repository
-  sudo bash -c 'cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-  deb https://apt.kubernetes.io/ kubernetes-xenial main
-  EOF'
+#Add the Kubernetes apt repository
+sudo bash -c 'cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF'
 
-  #Update the package list 
-  sudo apt-get update
-  #apt-cache policy kubelet | head -n 20 
+#Update the package list 
+sudo apt-get update
+#apt-cache policy kubelet | head -n 20 
 
-  #Install the required packages, if needed we can request a specific version. 
-  #Pick the same version you used on the Control Plane Node in 0-PackageInstallation-containerd.sh
-  VERSION=1.24.3-00
-  sudo apt-get install -y kubelet=$VERSION kubeadm=$VERSION kubectl=$VERSION
-  sudo apt-mark hold kubelet kubeadm kubectl containerd.io
+#Install the required packages, if needed we can request a specific version. 
+#Pick the same version you used on the Control Plane Node in 0-PackageInstallation-containerd.sh
+VERSION=1.24.3-00
+sudo apt-get install -y kubelet=$VERSION kubeadm=$VERSION kubectl=$VERSION
+sudo apt-mark hold kubelet kubeadm kubectl containerd.io
 
-  #Ensure both are set to start when the system starts up.
-  sudo systemctl enable kubelet.service
-  sudo systemctl enable containerd.service
+#Ensure both are set to start when the system starts up.
+sudo systemctl enable kubelet.service
+sudo systemctl enable containerd.service
 
-  echo "kubernetes binaries all installed.... You are ready to manually initialize the cluster."
+echo "kubernetes binaries all installed.... You are ready to manually initialize the cluster."
 
-  touch .binariesdone
+touch .binariesdone
 
   if [[ $(hostname) == "my-ubuntu-1" ]]; then
 
@@ -167,7 +171,7 @@ EOF
 
   fi
 
-  echo "If you want this script initialize the cluster, launch it again and it will do the following for you: initialize it, set-up your kubectl, and process the calico file for you"
+echo "If you want this script initialize the cluster, launch it again and it will do the following for you: initialize it, set-up your kubectl, and process the calico file for you"
 
 fi
 
